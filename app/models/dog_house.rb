542e355336464a06516a7228b5c24a05f6bcdd68
GLADYS <mungaiglado106@gmail.com>
2022-09-27 19:50:31 +0300

    inital commit

--- app/models/dog_house.rb
+++ app/models/dog_house.rb
@@ -1,3 +1,3 @@
 class DogHouse < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy # tells the A.R to delete  all the associated  records when the parent is deleted.
 end
